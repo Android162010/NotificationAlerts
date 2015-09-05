b35fb60dfd755001607f70970a4e4638441eaf1b
Blaine <bstreet162010@yahoo.com>
2015-09-05 00:42:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gizelem/notificationalerts/MoreInfoNotification.java
@@ -0,0 +1,19 @@
+package com.example.gizelem.notificationalerts;
+
+/**
+ * Created by GizeleM on 8/31/2015.
+ */
+
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+// Called when the notification is clicked on in the task bar
+public class MoreInfoNotification extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_info_notific);
+    }
+}
